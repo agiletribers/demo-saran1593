a87c074254cc4b71c3ee3f68236b5b4dae9a5b7d
saran1593 <saranz1593@gmail.com>
2026-03-09 12:17:28 +0530

    fix

--- embedings.py
+++ embedings.py
@@ -7,16 +7,17 @@ def embedding(text):
     result= model.encode(text)
     return result
 print(embedding("world"))
-# def cosine(a,b):
-#     return np.dot(a,b) / (np.linalg.norm(a)*np.linalg.norm(b))
+
+def cosine(a,b):
+    return np.dot(a,b) / (np.linalg.norm(a)*np.linalg.norm(b))
 
 
-# text1=embedding("car")
-# text2=embedding("fruit")
-# text3=embedding("apple")
+text1=embedding("car")
+text2=embedding("fruit")
+text3=embedding("apple")
 
 
-# output=cosine(text1,text2)
-# output2=cosine(text2,text3)
-# print(output)
-# print(output2)
+output=cosine(text1,text2)
+output2=cosine(text2,text3)
+print(output)
+print(output2)
