08c27106f64c3f64e5478fab1e347a87c62ee5a4
saran <saranz1593@gmail.com>
2026-03-09 12:14:56 +0530

    Add files via upload

---
+++ embedings.py
@@ -0,0 +1,22 @@
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def embedding(text):
+    result= model.encode(text)
+    return result
+print(embedding("world"))
+# def cosine(a,b):
+#     return np.dot(a,b) / (np.linalg.norm(a)*np.linalg.norm(b))
+
+
+# text1=embedding("car")
+# text2=embedding("fruit")
+# text3=embedding("apple")
+
+
+# output=cosine(text1,text2)
+# output2=cosine(text2,text3)
+# print(output)
+# print(output2)
